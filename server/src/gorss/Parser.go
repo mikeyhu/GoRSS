aaacbb204776d227236db5c89d5a210ce786d777
Michael Hughes <mikee.hughes@gmail.com>
2014-08-23 21:59:40 +0100

    Basic main added

--- server/src/gorss/Parser.go
+++ server/src/gorss/Parser.go
@@ -1,1 +1,35 @@
-package gorss
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"gorss/rss"
+)
+
+
+func main() {
+	resp, err := http.Get("http://feeds.bbci.co.uk/news/video_and_audio/uk/rss.xml")
+	if err != nil {
+		// handle error
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+
+	rssBody := string(body)
+	//println(rssBody)
+	parsed,_ := rss.Parse(rssBody)
+
+	for _,story := range rss.Normalise(parsed) {
+		fmt.Printf("Title: %v\n",story.Title)
+	}
+
+}
+
+
+
