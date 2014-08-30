90ccd4bc4e6e381af58deda5c5d78cba797276dd
Michael Hughes <mikee.hughes@gmail.com>
2014-08-30 20:59:27 +0100

    Added gocheck to another set of tests

--- server/src/gorss/controllers/controller_test.go
+++ server/src/gorss/controllers/controller_test.go
@@ -23,26 +23,25 @@ func (s *ControllerSuite) SetUpTest(c *C) {
 }
 
 func (s *ControllerSuite) TestLatestStoriesHandler_ReturnsEmpty(c *C) {
-
+	//Given
 	resp := httptest.NewRecorder()
-
 	uri := "/stories/latest"
 
 	req, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		c.Fatal(err)
 	}
-
+	//When
 	LatestStoriesHandler(resp, req)
 
+	//Then
 	c.Assert(resp.Code, Equals, http.StatusOK)
 	c.Assert(resp.Body.String(), Equals, "[]")
 }
 
 func (s *ControllerSuite) TestLatestStoriesHandler_ReturnsAnItem(c *C) {
-
+	//Given
 	resp := httptest.NewRecorder()
-
 	uri := "/stories/latest"
 
 	req, err := http.NewRequest("GET", uri, nil)
@@ -53,13 +52,13 @@ func (s *ControllerSuite) TestLatestStoriesHandler_ReturnsAnItem(c *C) {
 	var testStory = domain.Story{
 		Title: "Another story",
 		Id:    "another_story"}
-
 	stories := []domain.Story{testStory}
-
 	storyRepo.Insert(stories)
 
+	//When
 	LatestStoriesHandler(resp, req)
 
+	//Then
 	c.Assert(resp.Code, Equals, http.StatusOK)
 	c.Assert(
 		resp.Body.String(),
--- server/src/gorss/state/feedRepo_test.go
+++ server/src/gorss/state/feedRepo_test.go
@@ -1,49 +1,47 @@
 package state
 
 import (
+	. "gopkg.in/check.v1"
 	"gorss/domain"
 	"testing"
 )
 
-var testFeed1 = domain.Feed{
-	Url:  "http://localhost:12345/rss.xml",
-	Tags: []string{"News", "Technology"}}
+func Test(t *testing.T) { TestingT(t) }
 
-var testFeed2 = domain.Feed{
-	Url:  "http://localhost:54321/rss.xml",
-	Tags: []string{"News", "Technology"}}
+type FeedRepoSuite struct{}
 
-func TestFeedRepo_Insert(t *testing.T) {
+var _ = Suite(&FeedRepoSuite{})
+
+func (s *FeedRepoSuite) SetUpTest(c *C) {
 	clearCollection(COLLECTION_FEEDS)
-	feeds := []domain.Feed{
-		testFeed1,
-		testFeed2}
-
-	repo := GetFeedRepo(CONNECTION)
-
-	err := repo.Insert(feeds)
-
-	if err != nil {
-		t.Errorf("FeedRepo.Insert() returned %v", err)
-	}
 }
 
-func TestFeedRepo_All(t *testing.T) {
-	clearCollection(COLLECTION_FEEDS)
-	feeds := []domain.Feed{
-		testFeed1,
-		testFeed2}
+var testFeeds = []domain.Feed{
+	domain.Feed{
+		Url: "http://localhost:12345/rss.xml", Tags: []string{"News", "Technology"}},
+	domain.Feed{
+		Url: "http://localhost:54321/rss.xml", Tags: []string{"News", "Technology"}}}
 
+func (s *FeedRepoSuite) TestFeedRepo_Insert(c *C) {
+	//Given
 	repo := GetFeedRepo(CONNECTION)
 
-	err := repo.Insert(feeds)
+	//When
+	err := repo.Insert(testFeeds)
 
+	//Then
+	c.Assert(err, IsNil)
+}
+
+func (s *FeedRepoSuite) TestFeedRepo_All(c *C) {
+	//Given
+	repo := GetFeedRepo(CONNECTION)
+	err := repo.Insert(testFeeds)
+
+	//When
 	result, err := repo.All()
 
-	if err != nil {
-		t.Errorf("FeedRepo.All() returned %v", err)
-	}
-	if len(result) != 2 {
-		t.Errorf("FeedRepo.All() returned %v", result)
-	}
+	//Then
+	c.Assert(err, IsNil)
+	c.Assert(len(result), Equals, 2)
 }
