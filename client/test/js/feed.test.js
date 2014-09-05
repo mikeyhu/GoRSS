432936e22dfb9773d2023502cc0772a578f2a1a7
Michael Hughes <mikee.hughes@gmail.com>
2014-09-05 20:44:18 +0100

    HttpBackend fake to check getting and adding feeds

--- client/test/js/feed.test.js
+++ client/test/js/feed.test.js
@@ -2,16 +2,42 @@ describe("Testing the feed",function() {
 
   beforeEach(module("gorssApp"));
 
-  var ctrl, scope;
+  var ctrl, scope, httpBackend;
 
-  beforeEach(inject(function($controller, $rootScope){
+  beforeEach(inject(function($controller, $rootScope, $httpBackend){
     scope = $rootScope;
+    httpBackend = $httpBackend;
+
+    httpBackend.whenGET("../feeds/").respond([
+      {Url:"A", Tags:["tag"]},
+      {Url:"B", Tags:["tag"]}
+      ]);
+
     ctrl = $controller("feedController", {
       $scope : scope
     });
   }));
 
+
   it("should say hello", function(){
-    expect(scope.feedCount()).toBe(0);
+
+    httpBackend.flush();
+    expect(scope.feedCount()).toBe(2);
+  });
+
+  it("should be able to post content", function(){
+
+    httpBackend.expectPOST("../feeds/").respond();
+
+    scope.feedUrl = "http://mike.was.here";
+    scope.feedTags = "tag";
+    scope.addFeed();
+
+    httpBackend.flush();
+
+    expect(scope.feedUrl).toBe("");
+    expect(scope.feedTags).toBe("");
+    expect(scope.feedCount()).toBe(3);
+    expect(scope.feeds[2].Url).toBe("http://mike.was.here")
   });
 });
