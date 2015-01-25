5428cbeac83307438b8ad5d5eaebb3b5bdbe96fc
Michael Hughes <mikee.hughes@gmail.com>
2015-01-25 19:48:39 +0000

    Client renders favorite star

--- client/src/js/story.js
+++ client/src/js/story.js
@@ -1,3 +1,6 @@
+var STATE_FAVORITE = "favorite";
+var STATE_NEW = "new";
+
 app.controller('storyController', function ($scope, $http) {
   "use strict";
 
@@ -14,4 +17,18 @@ app.controller('storyController', function ($scope, $http) {
       data.unshift("select a tag");
       $scope.tags = data;
     });
+
+  $scope.toggleFavorite = function (index) {
+    var story = $scope.stories[index];
+    story.State = (story.State === STATE_FAVORITE) ? STATE_NEW : STATE_FAVORITE;
+  };
+
+  $scope.isFavorite = function (index) {
+    var story = $scope.stories[index];
+    return (story.State === STATE_FAVORITE);
+  };
+
+  $scope.starType = function (favorite) {
+    return favorite ? "glyphicon-star" : "glyphicon-star-empty";
+  };
 });
--- client/test/js/story.test.js
+++ client/test/js/story.test.js
@@ -13,10 +13,10 @@ describe("Stories can be displayed", function () {
       {Title: "A story", Link: "A link", Date: Date.now()}
     ]);
 
-    httpBackend.whenGET("../stories/tags").respond(["news","technology"]);
+    httpBackend.whenGET("../stories/tags").respond(["news", "technology"]);
 
     $controller("storyController", {
-      $scope : scope
+      $scope: scope
     });
   }));
 
@@ -32,5 +32,63 @@ describe("Stories can be displayed", function () {
     expect(scope.tags[1]).toBe("news");
     expect(scope.tags[2]).toBe("technology");
   });
+});
 
+describe("Stories with state", function () {
+  "use strict";
+
+  beforeEach(module("gorssApp"));
+
+  var scope, httpBackend;
+
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+    scope = $rootScope;
+    httpBackend = $httpBackend;
+
+    httpBackend.whenGET("../stories/latest").respond([
+      {Title: "A story", Link: "A link", Date: Date.now(), State: "new"},
+      {Title: "Another story", Link: "A link", Date: Date.now(), State: "favorite"}
+    ]);
+
+    httpBackend.whenGET("../stories/tags").respond(["news", "technology"]);
+
+    $controller("storyController", {
+      $scope: scope
+    });
+  }));
+
+
+  it("should mark new stories as not favorited", function () {
+    httpBackend.flush();
+    expect(scope.stories.length).toBe(2);
+    expect(scope.isFavorite(0)).toBe(false);
+    expect(scope.isFavorite(1)).toBe(true);
+  });
+
+  it("should mark favorite stories as favorited", function () {
+    httpBackend.flush();
+    expect(scope.isFavorite(1)).toBe(true);
+  });
+
+  it("should be able to toggle on a new story", function () {
+    httpBackend.flush();
+    scope.toggleFavorite(0);
+    expect(scope.isFavorite(0)).toBe(true);
+  });
+
+  it("should be able to toggle off a favorite story", function () {
+    httpBackend.flush();
+    scope.toggleFavorite(1);
+    expect(scope.isFavorite(1)).toBe(false);
+  });
+
+  it("should show new stories with empty star", function () {
+    httpBackend.flush();
+    expect(scope.starType(false)).toBe("glyphicon-star-empty");
+  });
+
+  it("should show favorite stories with filled star", function () {
+    httpBackend.flush();
+    expect(scope.starType(true)).toBe("glyphicon-star");
+  });
 });
